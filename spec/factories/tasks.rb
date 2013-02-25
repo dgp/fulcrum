8591f9d6315b5289eb6083d7d0de3ff17ecffc4e
Gustavo Chain <gchain@gmail.com>
2013-02-25 15:42:48 -0300

    Added basic specs

--- spec/factories/tasks.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :task do
-    story nil
-    task "MyString"
-    done false
-  end
-end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:project) { mock_model(Project, :suppress_notifications => true) }
+  let(:story)   { mock_model(Story, :project => project) }
+  
+  subject { Factory.build :task, :story => story }
+
+  describe "validations" do
+    
+    describe "#name" do
+      before { subject.task = '' }
+      it { should have(1).error_on(:task) }
+    end
+
+  end
+
+  describe "#as_json" do
+
+    it "returns the right keys" do
+      subject.as_json["task"].keys.sort.should == %w[
+        created_at done id story_id task updated_at
+      ]
+    end
+
+  end
 end
